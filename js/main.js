f949eead9a0b15ad557f3ae35ce489410553896c
Yahya <woXrooX@gmail.com>
2022-06-27 18:15:23 +0200

    v2

--- js/main.js
+++ js/main.js
@@ -1,64 +1,120 @@
 'use strict';
 
-//////// nav
-function navSetActive(){
-  let validHash = false;
+//////// Menu
+Menu: {
+  const menu = document.querySelector("body > menu");
+  let disableObserving = false;
 
-  // Every a in nav
-  document.querySelectorAll("body > nav > a").forEach((a) => {
-    // Default Expected Condition
-    if(a.getAttribute("href") == window.location.hash){
-      a.setAttribute("active", "");
-      setTimeout(()=>{
-        document.querySelector(`body > section${a.getAttribute("href")}`).removeAttribute("style");
-      }, 250);
+  // Open On Button Click
+  document.querySelector("body > button").addEventListener("click", ()=>{
+    menu.classList.add("open");
 
+  });
+
+  // Disable Observing On Click menu > a
+  document.querySelectorAll("body > menu > a").forEach((a)=>{
+    a.onclick = ()=>{
+      disableObserving = true;
+    }
+  });
+
+
+  // Close On Click The Cover
+  menu.addEventListener("click", ()=>{
+    menu.classList.remove("open");
+
+  });
+
+  // Sets Active Menu And Checks If Hash Is Valid And Updates Title
+  function update(){
+    let validHash = false;
+
+    // Every a in Menu
+    document.querySelectorAll("body > menu > a").forEach((a) => {
+      // Default Expected Condition
+      if(a.getAttribute("href") == window.location.hash){
+        a.setAttribute("active", "");
+
+        validHash = true;
+
+      }
+
+      // Removing "active" Attribute From menu > a
+      else{
+        a.removeAttribute("active");
+
+      }
+
+    });
+
+    // If Hash Is Invalid Set It To Home
+    if(validHash == false){
+      window.location.hash = "home";
+      document.querySelector("body > menu > a:nth-child(1)").setAttribute("active", "");
 
       validHash = true;
+
     }
 
-    // Removing "active" Attribute From nav > a
-    else{
-      a.removeAttribute("active");
-      setTimeout(()=>{
-        document.querySelector(`body > section${a.getAttribute("href")}`).style.display = "none";
-      }, 250);
-    }
+    // Updating Document Title
+    document.title = "woXrooX | "+window.location.hash.replace('#', '').toUpperCase();
 
-  });
-
-  // If Hash Is Invalid Set It To Home
-  if(validHash == false){
-    window.location.hash = "home";
-    document.querySelector("body > nav > a:nth-child(1)").setAttribute("active", "");
-
-    validHash = true;
-
+    // Allowing Again Observing Not Great Way But Works Untill I Find New Solution
+    setTimeout(()=>{
+      disableObserving = false;
+    }, 500);
   }
 
-  // Updating Document Title
-  document.title = "woXrooX | "+window.location.hash.replace('#', '').toUpperCase();
+  // Could Add Few More Ifs To Catch ReadyState
+  document.addEventListener('readystatechange', update);
+  window.addEventListener('hashchange', update);
+
+  // Update Hash On Scroll
+  const observer = new IntersectionObserver(elements =>{
+    elements.forEach((element)=>{
+      if(element.isIntersecting && disableObserving == false){
+        window.location.hash = element.target.id;
+      }
+    });
+
+  }, {
+    threshhold: 0,
+    rootMargin: "-50px"
+  });
+
+  document.querySelectorAll("body > main > div").forEach((element)=>{observer.observe(element);});
 }
 
-// Could add few more ifs to catch readyState
-document.addEventListener('readystatechange', navSetActive);
-window.addEventListener('hashchange', navSetActive);
+// let divs = document.querySelectorAll("body > main > div");
+// window.addEventListener("scroll", ()=>{
+//   divs.forEach((div)=>{
+//     if(window.scrollY > div.offsetTop && window.scrollY < (div.offsetHeight + div.offsetTop)){
+//       console.log("In", window.scrollY, div.id);
+//       window.location.hash = div.id;
+//     }else{
+//       console.log("Out", window.scrollY, div.id);
+//     }
+//   });
+//   console.log(window.scrollY);
+// });
 
-//////// observer
-const targets = document.querySelectorAll(".observerTarget");
-let delay = 0;
-const observer = new IntersectionObserver(elements =>{
-  elements.forEach((element)=>{
-    delay += 20;
-    setTimeout(()=>{
+
+
+//////// Observer
+Observer: {
+  const observer = new IntersectionObserver(elements =>{
+    elements.forEach((element)=>{
       element.target.classList.toggle("fadeIn", element.isIntersecting);
-    }, delay);
-  });
-  delay = 0;
-}, {
-  threshhold: 0
-});
 
-targets.forEach((target)=>{
-  observer.observe(target);
-});
+      // Disabling Second Time Observing
+      if(element.isIntersecting) observer.unobserve(element.target);
+
+    });
+  }, {
+    threshhold: 0
+    // rootMargin: "-50px"
+  });
+
+  document.querySelectorAll(".observerTarget").forEach((element)=>{observer.observe(element);});
+
+}
