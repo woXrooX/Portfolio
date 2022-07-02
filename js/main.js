c2d07079f94cad08a0e2410ac155f2c014d34b92
Yahya <woXrooX@gmail.com>
2022-07-02 16:44:01 +0200

    v3 Live Beta

--- js/main.js
+++ js/main.js
@@ -1,128 +1,45 @@
 'use strict';
 
-//////// Menu
-Menu: {
-  const menu = document.querySelector("body > menu");
-  let disableObserving = false;
-
-  // Open On Button Click
-  document.querySelector("body > button").addEventListener("click", ()=>{
-    menu.classList.add("open");
-
-  });
-
-  // Disable Observing On Click menu > a
-  document.querySelectorAll("body > menu > a").forEach((a)=>{
-    a.onclick = ()=>{
-      disableObserving = true;
-    }
-  });
-
-
-  // Close On Click The Cover
-  menu.addEventListener("click", ()=>{
-    menu.classList.remove("open");
-
-  });
-
-  // Sets Active Menu And Checks If Hash Is Valid And Updates Title
-  function update(){
-    let validHash = false;
-
-    // Every a in Menu
-    document.querySelectorAll("body > menu > a").forEach((a) => {
-      // Default Expected Condition
-      if(a.getAttribute("href") == window.location.hash){
-        a.setAttribute("active", "");
-
-        validHash = true;
-
-      }
-
-      // Removing "active" Attribute From menu > a
-      else{
-        a.removeAttribute("active");
-
-      }
-
-    });
-
-    // If Hash Is Invalid Set It To Home
-    if(validHash == false){
-      window.location.hash = "home";
-      document.querySelector("body > menu > a:nth-child(1)").setAttribute("active", "");
-
-      validHash = true;
-
-    }
-
-    // Updating Document Title
-    document.title = "woXrooX | "+window.location.hash.replace('#', '').toUpperCase();
-
-    // Allowing Again Observing Not Great Way But Works Untill I Find New Solution
-    setTimeout(()=>{
-      disableObserving = false;
-    }, 500);
-  }
-
-  // Could Add Few More Ifs To Catch ReadyState
-  document.addEventListener('readystatechange', update);
-
-  // On Hash Change
-  window.addEventListener('hashchange', update);
-
-  // Update Hash On Scroll
-  const observer = new IntersectionObserver(elements =>{
-    elements.forEach((element)=>{
-      if(element.isIntersecting && disableObserving == false){
-        window.location.hash = element.target.id;
-      }
-    });
-
-  }, {
-    threshhold: 0,
-    rootMargin: "-50px"
-  });
-
-  document.querySelectorAll("body > main > div").forEach((element)=>{observer.observe(element);});
-}
+import Navigation from "./navigation.js";
 
 //////// Elements Fade In Move Top -> ".observerTarget"
-ObserveByClass: {
-  const observer = new IntersectionObserver(elements =>{
-    elements.forEach((element)=>{
-      element.target.classList.toggle("fadeIn", element.isIntersecting);
-
-      // Disabling Second Time Observing
-      if(element.isIntersecting) observer.unobserve(element.target);
-
-    });
-  }, {
-    threshhold: 0
-    // rootMargin: "-50px"
-  });
-
-  document.querySelectorAll(".observerTarget").forEach((element)=>{observer.observe(element);});
-
-}
+// ObserveByClass: {
+//   const observer = new IntersectionObserver(elements =>{
+//     elements.forEach((element)=>{
+//       element.target.classList.toggle("fadeIn", element.isIntersecting);
+//
+//       // Disabling Second Time Observing
+//       // if(element.isIntersecting) observer.unobserve(element.target);
+//
+//     });
+//   }, {
+//     threshhold: 0
+//     // rootMargin: "-50px"
+//   });
+//
+//   document.querySelectorAll(".observerTarget").forEach((element)=>{
+//   // document.querySelectorAll(".observerTarget").forEach((element)=>{
+//     observer.observe(element);});
+//
+// }
 
 //////// Skills
-Skills: {
-  const observer = new IntersectionObserver(elements =>{
-    elements.forEach((element)=>{
-
-      if(element.isIntersecting) {
-        element.target.style.width = element.target.getAttribute("percentage");
-        element.target.parentNode.previousElementSibling.innerHTML = `( ${element.target.getAttribute("percentage")} )`;
-      }else{
-        element.target.style.width = "0px";
-      }
-
-    });
-  }, {
-    threshhold: 0
-    // rootMargin: "-50px"
-  });
-
-  document.querySelectorAll("body > main > div#skills > main > div > div").forEach((element)=>{observer.observe(element);});
-}
+// Skills: {
+//   const observer = new IntersectionObserver(elements =>{
+//     elements.forEach((element)=>{
+//
+//       if(element.isIntersecting) {
+//         element.target.style.width = element.target.getAttribute("percentage");
+//         element.target.parentNode.previousElementSibling.innerHTML = `( ${element.target.getAttribute("percentage")} )`;
+//       }else{
+//         element.target.style.width = "0px";
+//       }
+//
+//     });
+//   }, {
+//     threshhold: 0
+//     // rootMargin: "-50px"
+//   });
+//
+//   document.querySelectorAll("body > section#skills > main > div > div").forEach((element)=>{observer.observe(element);});
+// }
---
+++ js/navigation.js
@@ -0,0 +1,269 @@
+export default class Navigation{
+  // Variables. No Static Const Support Yet :(
+  static #background = document.querySelector("body > background");
+  static #backgroundImage = Navigation.#background.querySelector("img");
+  static #transitionVelocity = parseInt(getComputedStyle(document.body).getPropertyValue('--transition-velocity'));
+  static #menuButton = document.querySelector("body > button");
+  static #menu = document.querySelector("body > menu");
+  static #anchors = document.querySelectorAll("body > menu > a");
+
+  static #hashes = [];
+  static #isMoreShown = false;
+  static #pageTransitionDelay = 500;
+
+
+  static {
+    Navigation.#collectHashes();
+    Navigation.#onLoad();
+    Navigation.#onHashchange();
+
+    Navigation.#onWheel();
+    Navigation.#onTouchmove();
+
+    Navigation.#menuShow();
+    Navigation.#menuHide();
+
+    Navigation.#onClickShowMoreButton();
+
+  }
+
+  /////// Inits
+  static #collectHashes(){
+    Navigation.#anchors.forEach((a)=>{
+      Navigation.#hashes.push(a.hash);
+
+      a.addEventListener("click", Navigation.#showLess);
+
+    });
+  }
+
+  // On Load
+  static #onLoad(){
+    document.addEventListener('readystatechange', ()=>{
+      // Execute "navigate" On First Load
+      Navigation.#navigate();
+    });
+  }
+
+  // On Hash Change
+  static #onHashchange(){
+    window.addEventListener('hashchange', ()=>{
+      event.preventDefault();
+
+      Navigation.#navigate();
+    });
+  }
+
+
+  /////// Navigation Using  Wheel / Scroll / Key Press
+  static #onWheel(){
+    let previousTime = Date.now();
+    let currentTime;
+    let differenceTime;
+    window.addEventListener('wheel', ()=>{
+      // Check If Any More Is Shown If So Let User Scroll
+      if(Navigation.#isMoreShown) return;
+
+      // Detect Last Wheel Detection.
+      // If Last Wheel Detection Was < "pageTransitionDelay" Then Do Nothing
+      currentTime = Date.now();
+      differenceTime = currentTime - previousTime;
+      if(differenceTime < Navigation.#pageTransitionDelay) return;
+      previousTime = currentTime;
+
+      let currentHashId = Navigation.#hashes.indexOf(window.location.hash) || 0;
+      let idPlusMinus = null;
+
+      if(event.deltaY > 0 && Navigation.#hashes[currentHashId+1] != undefined) idPlusMinus = 1;
+      else if(event.deltaY < 0 && Navigation.#hashes[currentHashId-1] != undefined) idPlusMinus = -1;
+
+      // Update The Hash And "hashchange" Event Also Will Be Invoked Automatically
+      window.location.href = Navigation.#hashes[currentHashId+idPlusMinus];
+
+    });
+
+  }
+
+  static #onTouchmove(){
+    let touches = [];
+    let firstTouchPageY = null;
+
+    let lastActionTime = Date.now();
+    let currentTime;
+    let differenceTime;
+
+    document.addEventListener("touchstart", ()=>{
+      // Check If Any More Is Shown If So Let User Scroll
+      if(Navigation.#isMoreShown) return;
+
+      firstTouchPageY = event.touches[0].pageY;
+
+    });
+
+    document.addEventListener("touchmove", ()=>{
+      // Check If Any More Is Shown If So Let User Scroll
+      if(Navigation.#isMoreShown) return;
+
+      // Detect Last "touchmove"
+      currentTime = Date.now();
+      differenceTime = currentTime - lastActionTime;
+      if(differenceTime < Navigation.#pageTransitionDelay / 2) return;
+      lastActionTime = currentTime;
+
+      let currentHashId = Navigation.#hashes.indexOf(window.location.hash) || 0;
+      let idPlusMinus = null;
+
+      if((firstTouchPageY - event.touches[0].pageY) > 0 && Navigation.#hashes[currentHashId+1] != undefined) idPlusMinus = 1;
+      else if((firstTouchPageY - event.touches[0].pageY) < 0 && Navigation.#hashes[currentHashId-1] != undefined) idPlusMinus = -1;
+
+      window.location.href = Navigation.#hashes[currentHashId+idPlusMinus];
+
+    });
+
+  }
+
+
+  /////// Menu
+  // Menu Show On Button Click
+  static #menuShow(){
+    Navigation.#menuButton.addEventListener("click", ()=>{
+      Navigation.#menu.classList.add("show");
+
+    });
+  }
+
+  // Menu Hide On Click The Cover. "menu = cover"
+  static #menuHide(){
+    Navigation.#menu.addEventListener("click", ()=>{
+      Navigation.#menu.classList.remove("show");
+
+    });
+  }
+
+
+  /////// Hash
+  // Sets Active Menu And Checks If Hash Is Valid And Updates Title
+  static #navigate(){
+    let validHash = false;
+
+    // Every a in Menu
+    Navigation.#anchors.forEach((a) => {
+      // Default Expected Condition
+      if(a.getAttribute("href") == window.location.hash){
+        a.setAttribute("active", "");
+
+        validHash = true;
+
+      }
+
+      // Removing "active" Attribute From menu > a
+      else a.removeAttribute("active");
+
+    });
+
+    // If Hash Is Invalid Set Hash To Home And Activate "a" of Home In "menu"
+    if(validHash == false){
+      window.location.hash = "home";
+      Navigation.#anchors[0].setAttribute("active", "");
+
+      validHash = true;
+
+    }
+
+    // Updating Document Title
+    document.title = "woXrooX | "+window.location.hash.replace('#', '').toUpperCase();
+
+  }
+
+
+  /////// Show More/Less
+  // Listen To Click On "Show More" Button
+  static #onClickShowMoreButton(){
+    document.querySelectorAll("body > section > footer > button").forEach((element) => {
+      element.addEventListener("click", ()=>{
+        if(Navigation.#isMoreShown){
+          // Update Button Text To "Continue Reading"
+          element.innerText = "Continue Reading";
+
+          Navigation.#showLess();
+
+        }else{
+          // Update Button Text To "Back To Navigation"
+          element.innerText = "Back To Navigation";
+
+          Navigation.#showMore();
+
+        }
+
+      });
+    });
+
+  }
+
+  // Show More
+  static #showMore(){
+    // Check If Alread Shown More
+    if(Navigation.#isMoreShown == true) return;
+
+    // Update "isMoreShown" To "true"
+    Navigation.#isMoreShown = true;
+
+    // Current Section + .scrollable
+    let section = document.getElementById(window.location.hash.replace('#', ''));
+    section.classList.add("scrollable");
+
+    // <more/> + .show
+    section.querySelector("more").classList.add("show");
+
+    // Background Image
+    Navigation.#backgroundImage.classList.add("moreShown");
+    Navigation.#background.classList.add("moreShown");
+
+    // Scroll 100 To Bottom After "--transition-velocity"
+    setTimeout(()=>{
+      section.scrollTo({
+        top: 100,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }, Navigation.#transitionVelocity / 4);
+
+  }
+
+  // Show Less
+  static #showLess(){
+    // Check If Alread Shown Less
+    if(Navigation.#isMoreShown == false) return;
+
+    // Update "isMoreShown" To "false"
+    Navigation.#isMoreShown = false;
+
+    // Current Section
+    let section = document.getElementById(window.location.hash.replace('#', ''));
+
+    // Wait "--transition-velocity"ms To Prevent Unsmooth Jump
+    setTimeout(()=>{
+      // section - .scrollable
+      section.classList.remove("scrollable");
+
+      // <more/> - .show
+      section.querySelector("more").classList.remove("show");
+    }, Navigation.#transitionVelocity);
+
+
+
+    // Background Image
+    Navigation.#backgroundImage.classList.remove("moreShown");
+    Navigation.#background.classList.remove("moreShown");
+
+    // Scroll Back To Top
+    section.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+
+  }
+
+
+}
